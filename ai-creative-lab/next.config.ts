5ba3307264b7db4a333be1abd34a3471acc35128
D Hamasaki <daisuke.hamu3@gmail.com>
2025-03-22 10:24:27 +0900

    change servidce name

--- ai-creative-lab/next.config.ts
+++ ai-creative-lab/next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   output: "standalone", // SSRモードを有効化
   images: {
     domains: ['localhost', 'tokyo-ai-classschool.netlify.app'], // 必要に応じてドメインを追加
-    unoptimized: false, // 画像最適化を有効化
+    unoptimized: true, // 画像最適化を無効化
   },
 };
 
